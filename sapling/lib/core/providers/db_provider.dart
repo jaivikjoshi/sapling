813653ff3f53d36f9e5577ba2449f80666b70cf7
jaivikjoshi <jaivikjoshi7@gmail.com>
2026-06-16 02:39:15 -0400

    chore: remove legacy duplicate app code

--- sapling/lib/core/providers/db_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../data/db/sapling_database.dart';
-
-final databaseProvider = Provider<SaplingDatabase>((ref) {
-  final db = SaplingDatabase();
-  ref.onDispose(() => db.close());
-  return db;
-});
